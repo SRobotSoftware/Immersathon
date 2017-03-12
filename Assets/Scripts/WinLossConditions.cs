3cb831e8ba4b5fc29f5c4dc853b94ec6a5e39f7e
justinandrewfien <jfien12@live.com>
2017-03-11 18:33:27 -0700

    This still isn't quite there

--- Assets/Scripts/WinLossConditions.cs
+++ Assets/Scripts/WinLossConditions.cs
@@ -8,10 +8,16 @@ public class WinLossConditions : MonoBehaviour
     public bool GameOver;
     public bool Victory;
     public GameObject Flipper;
+    public Light Sun;
+    public void SunFlip()
+    {
+        Sun.transform.eulerAngles = new Vector3(270f, 0f, 0f);
+    }
+
 
     void Start()
     {
-        GameOver = true;
+        GameOver = false;
 
     }
 
@@ -19,7 +25,6 @@ public class WinLossConditions : MonoBehaviour
     {
         if (GameOver == true)
         {
-            Flipper.SunFlip();
             if (Victory == true)
             {
                 //Scene Transition to "You Win"
--- Assets/Scripts/WinLossTransitions.cs
+++ Assets/Scripts/WinLossTransitions.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class WinLossTransitions : MonoBehaviour
 {
-    public Light Sun;
+    
 
-    public void SunFlip()
-    {
-        Sun.transform.eulerAngles = new Vector3(270f, 0f, 0f);
-    }
+    
     // Use this for initialization
     void Start()
     {
