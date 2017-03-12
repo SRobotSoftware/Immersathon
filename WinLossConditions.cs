d949fd31bd2134c0ce05b477fa43578a4536b41d
justinandrewfien <jfien12@live.com>
2017-03-12 09:44:44 -0600

    Adding WinLossConditions.cs to Master Branch
    And Zack's not here to tell me not to. In your face, Zack...

---
+++ WinLossConditions.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinLossConditions : MonoBehaviour
+{
+    public int Score;
+    public bool GameOver;
+    public bool Victory;
+    public Light Sun;
+    public GameObject YouWin;
+    public GameObject YouLose;
+    public void SunFlip()
+    {
+        Sun.transform.eulerAngles = new Vector3(270f, 0f, 0f);
+    }
+
+
+    void Start()
+    {
+        GameOver = false;
+        YouWin.SetActive(false);
+        YouLose.SetActive(false);
+
+
+    }
+
+    void Update()
+    {
+        if (GameOver == true)
+        {
+            if (Victory == true)
+            {
+                YouWin.SetActive(true);
+                SunFlip();
+                //Scene Transition to "You Win"
+            }
+            else if (Victory == false)
+            {
+                YouLose.SetActive(true);
+                SunFlip();
+                //Scene Transition to "You Lose"
+            }
+            else
+            {
+                GameOver = false;
+            }
+        }
+    }
+}
